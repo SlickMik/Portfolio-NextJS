bf8d0a008f356b50427a5b77129c94e247fd31b1
Ibrahim Khan <97199231+SlickMik@users.noreply.github.com>
2023-09-06 12:18:29 +0800

    outdate

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 
-import './globals.css'
+import '../styles/globals.css'
 import type { Metadata } from 'next'
 import Navbar from '@/Components/Navbar'
 import Top_Finder from '@/Components/Top_Finder'
